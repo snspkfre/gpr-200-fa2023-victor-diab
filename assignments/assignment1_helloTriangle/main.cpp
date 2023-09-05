f14e3f9e755414d6e411acfe80a29ecc628144bc
victor <victor.diab@mymail.champlain.edu>
2023-09-05 12:49:27 -0400

    Followed tutorial

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,6 +8,22 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float vertices[9] = {
+	 //x   //y  //x
+	-0.5, -0.5, 0.0,  //bottom left
+	 0.5, -0.5, 0.0,  //bottom right
+	 0.0,  0.5, 0.0   //top center
+};
+
+//main.cpp
+const char* vertexShaderSource = R"(
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	void main(){
+		gl_Position = vec4(vPos,1.0);
+	}
+)";
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -26,6 +42,43 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Create a new vertex shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+	}
+
+	// Creates a new shader of a given type.
+	// Possible types: GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, etc
+	// Returns id of the shader object
+	unsigned int createShader(GLenum shaderType, const char* sourceCode);
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
