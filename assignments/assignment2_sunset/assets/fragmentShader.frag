cac129181f4cb6453b9c222c187665b60508414b
victor <victor.diab@mymail.champlain.edu>
2023-09-12 12:43:04 -0400

    Added shader.h and shader.cpp to vd namespace

---
+++ assignments/assignment2_sunset/assets/fragmentShader.frag
@@ -0,0 +1,7 @@
+#version 450
+out vec4 FragColor;
+uniform vec3 _Color;
+uniform float _Brightness;
+void main(){
+	FragColor = vec4(_Color * _Brightness,1.0);
+}
--- assignments/assignment2_sunset/main.cpp
+++ assignments/assignment2_sunset/main.cpp
@@ -8,6 +8,8 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <vd/shader.h>
+
 unsigned int createShader(GLenum shaderType, const char* sourceCode);
 unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
 unsigned int createVAO(float* vertexData, int numVertices);
@@ -22,24 +24,6 @@ float vertices[9] = {
 	 0.5, -0.5, 0.0,
 	 0.0,  0.5, 0.0 
 };
-
-const char* vertexShaderSource = R"(
-	#version 450
-	layout(location = 0) in vec3 vPos;
-	void main(){
-		gl_Position = vec4(vPos,1.0);
-	}
-)";
-
-const char* fragmentShaderSource = R"(
-	#version 450
-	out vec4 FragColor;
-	uniform vec3 _Color;
-	uniform float _Brightness;
-	void main(){
-		FragColor = vec4(_Color * _Brightness,1.0);
-	}
-)";
 
 float triangleColor[3] = { 1.0f, 0.5f, 0.0f };
 float triangleBrightness = 1.0f;
@@ -71,7 +55,10 @@ int main() {
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init();
 
-	unsigned int shader = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+	std::string vertexShaderSource = vd::loadShaderSourceFromFile("assets/vertexShader.vert");
+	std::string fragmentShaderSource = vd::loadShaderSourceFromFile("assets/fragmentShader.frag");
+
+	unsigned int shader = createShaderProgram(vertexShaderSource.c_str(), fragmentShaderSource.c_str());
 	unsigned int vao = createVAO(vertices, 3);
 
 	glUseProgram(shader);
